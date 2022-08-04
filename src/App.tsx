c7551d9bf2a98692163c5ecf2e89668c514e8b02
Elias Forte <eliasforte.dev@gmail.com>
2022-08-03 21:25:16 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import {
   ChakraProvider,
 } from '@chakra-ui/react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages/home';
 import theme from './styles/globalStyles';
 
 const App: React.FC = () => (
-  <ChakraProvider theme={theme}>
-    Oi
-  </ChakraProvider>
+  <BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </ChakraProvider>
+  </BrowserRouter>
 );
 
 export default App;
